f88eff2cd8c47f39ba8342f9415df57213182d40
Matt Dolin <albeto4000@gmail.com>
2026-07-16 15:46:59 -0400

    Added requirements.txt file

--- restaurant_recommender/core/views.py
+++ restaurant_recommender/core/views.py
@@ -3,6 +3,7 @@ from .models import Restaurant, Review
 import ast
 
 def index(request):
+    #SELECT * FROM RESTAURANTS ORDER BY 'review_count' DESC LIMIT 10
     restaurant_list = Restaurant.objects.all().order_by('-review_count')[:10]
 
     attributes = [ast.literal_eval(restaurant.attributes) for restaurant in restaurant_list]
